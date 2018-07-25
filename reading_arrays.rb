1957118c2391127860d66f9aa71d3fb8ccc6b58f
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-25 15:12:50 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,31 +7,31 @@ STUDENT_NAMES = [
   "Rihanna"
   ]
 
-def first_student_by_index
-  puts STUDENT_NAMES[1]
-end
-first_student_by_index
+# def first_student_by_index
+#   puts STUDENT_NAMES[1]
+# end
+# first_student_by_index
 
-def fourth_student_by_index
-  puts STUDENT_NAMES[3]
-end
-fourth_student_by_index
+# def fourth_student_by_index
+#   puts STUDENT_NAMES[3]
+# end
+# fourth_student_by_index
 
-def last_student_by_index
-  puts STUDENT_NAMES.last
-end
-last_student_by_index
+# def last_student_by_index
+#   puts STUDENT_NAMES.last
+# end
+# last_student_by_index
 
 
-def first_student_by_method
-  puts STUDENT_NAMES.first
-end
-first_student_by_method
+# def first_student_by_method
+#   puts STUDENT_NAMES.first
+# end
+# first_student_by_method
 
-def last_student_by_method
-  puts STUDENT_NAMES.last 
-end
-last_student_by_method
+# def last_student_by_method
+#   puts STUDENT_NAMES.last 
+# end
+# last_student_by_method
 
 def first_second_and_third_students
   puts STUDENT_NAMES.first[1,2]
