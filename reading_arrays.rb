c7de4ddbf6e0df49b4775114b7550e4a2b4ae0b8
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-25 15:00:42 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,7 +8,7 @@ STUDENT_NAMES = [
   ]
 
 def first_student_by_index
-  puts STUDENT_NAMES[1]
+  puts STUDENT_NAMES[0]
 
   # Write a solution that returns the first student in the array student_names
 end
